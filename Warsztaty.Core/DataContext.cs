1be6d4b284fa4b1f50aaea2b958f39f72c2a02b3
Thirdcheer <228117@student.pwr.edu.pl>
2018-10-13 13:16:19 +0200

    Azure connection string

--- Warsztaty.Core/DataContext.cs
+++ Warsztaty.Core/DataContext.cs
@@ -10,7 +10,7 @@ namespace Warsztaty.Core
         public virtual DbSet<ContactModel> Contact { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=CodeAndCloud;Trusted_Connection=true;");
+            optionsBuilder.UseSqlServer(@"Server=tcp:warsztatykarolinadb.database.windows.net,1433;Initial Catalog=warsztatykarolinadb;Persist Security Info=False;User ID=dev7;Password=Passw0rd!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
