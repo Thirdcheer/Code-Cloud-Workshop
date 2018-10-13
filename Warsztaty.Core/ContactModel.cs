debe658b1825083d483726e20b70c8f8c3ab283c
Thirdcheer <228117@student.pwr.edu.pl>
2018-10-13 12:10:43 +0200

    Workshop app

---
+++ Warsztaty.Core/ContactModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Warsztaty.Core
+{
+    [Table("Contact")]
+    public class ContactModel
+    {
+        [Key]
+        public virtual int ID { get; set; }
+        [MaxLength(25)]
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ Warsztaty.Web/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Warsztaty.Core.Services;
+using Warsztaty.ViewModel;
+using Warsztaty.Web.Models;
+
+namespace Warsztaty.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContractViewModel model)
+        {
+            var contact = new ContactService();
+            contact.Add(model);
+            return RedirectToAction("Contact");
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
